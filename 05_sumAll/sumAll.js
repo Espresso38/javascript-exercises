25e873b93c4dbd27de00baadc42140cb3a47eeec
Espresso38 <naj.tomalak@gmail.com>
2023-07-03 16:23:49 +0200

    Solving problems and running tests on them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)) { 
+        return "ERROR";
+    }
+    else if (numOne > 0 && numTwo > 0) {
+        let sum = 0;
+        for (let i = 0; i <= numOne || i <= numTwo; i++) {
+            sum += i;
+        }
+    return sum;
+    }
+    else if (numOne < 0 || numTwo < 0) {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
